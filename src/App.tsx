d70949910ca575244a2d7e5806f6968b8a0f59ce
Masud <masudrahman.dev@gmail.com>
2024-03-07 19:01:12 +0600

    test

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+
+const App = () => {
+  return (
+    <div>
+
+      <article className="prose lg:prose-xl">
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p className="col-count-3 text-3xl">
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quaerat, repellendus. Molestiae ipsum repudiandae,
+          placeat odit autem repellat blanditiis sit, ea harum ex reprehenderit
+          ducimus velit numquam fugiat, voluptatem officiis pariatur.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked
+          to a series of rabies cases springing up around the country.
+        </p>
+      </article>
+      <select className="rounded-full px-4 py-3"></select>
+
+
+    </div>
+  );
+};
+
+export default App;
