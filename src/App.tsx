08f805f65a11007c946ba9420c193a8647ae52d4
Masud <masudrahman.dev@gmail.com>
2024-03-09 11:18:29 +0600

    cartified accountatn done

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import ProsMilestone from "./components/organisms/pros-milestone/ProsMilestone";
 import CookiesCard from "./components/organisms/cookies-card/CookiesCard";
 import TaxReturn2 from "./components/organisms/tax-return-2/TaxReturn2";
 import TaxReturn3 from "./components/organisms/tax-return-3/TaxReturn3";
+import CrossIcon from "./components/icons/cross-icon/CrossIcon";
+import Button from "./components/atoms/button/Button";
 
 const App = () => {
   return (
@@ -29,11 +31,23 @@ const App = () => {
         <div className="col-span-5   row-span-2   rounded bg-white p-base  ">
           <TaxReturn2 />
         </div>
-        <div className="group col-span-full row-span-1 bg-white p-base ">
+        <div className="group col-span-full row-span-1 rounded bg-white p-base ">
           <TaxReturn3 />
         </div>
-        <div className="col-span-4  row-span-1 rounded bg-white p-base ">
-          item7
+        <div className="col-span-4  row-span-1  bg-white ">
+          <div className="flex items-center justify-between rounded bg-primary p-6">
+            <div className="space-x-6 flex items-center">
+              <Button className="rounded-full bg-white px-4 py-2 text-primary">
+                CPA
+              </Button>
+              <p className="text-sm font-medium text-white">
+                Certified Public Accountant
+              </p>
+            </div>
+            <button className="inline-flex items-center justify-start rounded-full bg-white p-1 text-paragraph opacity-80">
+              <CrossIcon className="h-4 w-4 stroke-2" />
+            </button>
+          </div>
         </div>
         <div className="col-span-5 row-span-2  border ">item8</div>
         <div className="col-span-3  border ">item9</div>
